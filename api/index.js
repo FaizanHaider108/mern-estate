f927aec8e4e35864b3c965841298efeef2fec659
FaizanHaider108 <fiver1faizan@gmail.com>
2023-10-16 12:17:01 -0700

    chaing index

--- api/index.js
+++ api/index.js
@@ -1,48 +1,52 @@
 import express from "express";
 import mongoose from "mongoose";
-import userRoute from "../api/routes/user.route.js"
-import authRoute from "../api/routes/auth.user.js"
-import listingRoute from "../api/routes/listing.route.js"
+import userRoute from "./api/routes/user.route.js";
+import authRoute from "./api/routes/auth.user.js";
+import listingRoute from "./api/routes/listing.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
+
 dotenv.config();
-
-app.use( express.json() );
-app.use( cookieParser() );
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("DB is Connected !"))
-    .catch((err) => console.log(err));
-
 
 const app = express();
 const __dirname = path.resolve();
 
-const port = process.env.PORT || 3000; // Use the PORT environment variable if defined, otherwise use 3000.
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-} );
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("DB is Connected!"))
+    .catch((err) => console.error(err));
 
+const port = process.env.PORT || 3000;
 
+// Serve the static files
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
-// path APi Route
-app.use( "/api/user", userRoute );
-app.use( "/api/auth", authRoute );
-app.use( '/api/listing', listingRoute );
-app.use( express.static( path.join( __dirname, '/client/dist' ) ) );
+// API Routes
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use('/api/listing', listingRoute);
 
-app.get( '*', ( req, res ) => {
-    res.sendFile( path.join( __dirname, 'client', 'dist', 'index.html' ) );
-})
+// Catch-all route for SPA (Single Page Application) client-side routing
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
 
-app.use( ( err, req, res, next ) => {
+// Error handling middleware
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal server error";
-    return res.status( statusCode ).json( {
+    return res.status(statusCode).json({
         success: false,
         statusCode: statusCode,
         message: message
-    })
-})
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
